bab9422e8fcfbf0a4260e22f1a35ee9ceb258134
Vivek T <vivekthite@gmail.com>
2019-05-13 16:12:20 +0530

    modified deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,21 @@
-docker build -t vivekthite/multi-client:latest -t vivekthite/multi-client:$SHA -f ./client/Dockerfile ./client/
-docker build -t vivekthite/multi-server:latest -t vivekthite/multi-server:$SHA -f ./server/Dockerfile ./server/
-docker build -t vivekthite/multi-worker:latest -t vivekthite/multi-worker:$SHA -f ./worker/Dockerfile ./worker/
+docker build -t vivekthite/multi-client:latest  -f ./client/Dockerfile ./client
+docker build -t vivekthite/multi-server:latest  -f ./server/Dockerfile ./server
+docker build -t vivekthite/multi-worker:latest  -f ./worker/Dockerfile ./worker
+
+docker build -t vivekthite/multi-client:$SHA -f ./client/Dockerfile ./client/
+docker build -t vivekthite/multi-server:$SHA -f ./server/Dockerfile ./server/
+docker build -t vivekthite/multi-worker:$SHA -f ./worker/Dockerfile ./worker/
+
 docker push vivekthite/multi-client:latest
 docker push vivekthite/multi-server:latest
 docker push vivekthite/multi-worker:latest
+
 docker push vivekthite/multi-client:$SHA
 docker push vivekthite/multi-server:$SHA
 docker push vivekthite/multi-worker:$SHA
+
 kubectl apply -f k8s/
+
 kubectl set image deployments/server-deployment server=vivekthite/multi-server:$SHA
 kubectl set image deployments/client-deployment client=vivekthite/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=vivekthite/multi-worker:$SHA
